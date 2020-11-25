e586f0df39e7f73972994ca7dd57adb12666484a
zakayo.haule <zakayo.haule@ega.go.tz>
2020-11-25 13:30:29 +0300

    -start product management module

---
+++ Shopper/Migrations/20201124212046_AddedProductAttributeTableAndRemoveProductAttributeOptionTable.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Shopper.Migrations
+{
+    public partial class AddedProductAttributeTableAndRemoveProductAttributeOptionTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "product_attribute",
+                columns: table => new
+                {
+                    attribute_id = table.Column<ushort>(nullable: false),
+                    product_id = table.Column<uint>(nullable: false),
+                    created_at = table.Column<DateTime>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    updated_at = table.Column<DateTime>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.ComputedColumn)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_product_attribute", x => new { x.product_id, x.attribute_id });
+                    table.ForeignKey(
+                        name: "FK_product_attribute_attributes_attribute_id",
+                        column: x => x.attribute_id,
+                        principalTable: "attributes",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_product_attribute_products_product_id",
+                        column: x => x.product_id,
+                        principalTable: "products",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_product_attribute_attribute_id",
+                table: "product_attribute",
+                column: "attribute_id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "product_attribute");
+        }
+    }
+}

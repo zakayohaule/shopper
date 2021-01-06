6e7ada122ec76ec03d78c81abf3f7c8b1c92026f
zakayo.haule <zakayo.haule@ega.go.tz>
2021-01-06 11:17:05 +0300

    -generate invoice number from tenant code and date

---
+++ Shopper/Migrations/AdminAppDb/20210106052137_AddedCodeInTenantModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Shopper.Migrations.AdminAppDb
+{
+    public partial class AddedCodeInTenantModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "code",
+                table: "tenants",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "code",
+                table: "tenants");
+        }
+    }
+}
